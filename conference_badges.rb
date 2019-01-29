7637cf6c23c19a6ff117e70169d68ee53dd1b749
Luiz Pereira <luizfper@gmail.com>
2019-01-29 21:19:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  batch_badge=[]
+  array.each do |speaker|
+    batch_badge<<badge_maker(speaker)
+  end
+end
